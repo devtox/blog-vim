a7d403e6c6794cce604be5a3ccab18691f9ca21d
devtox <frank@fastmail.org>
2022-04-12 17:50:17 +0200

    update basic site

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ return [
     'production' => false,
     'baseUrl' => 'https://artisanstatic.netlify.app',
     'site' => [
-        'title' => 'My Jigsaw Blog',
-        'description' => 'Personal blog of John Doe.',
+        'title' => 'Vim.monster',
+        'description' => 'Blog of a vim fan.',
         'image' => 'default-share.png',
     ],
     'owner' => [
